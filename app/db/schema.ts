f5ddad5259c83fca837fc0395e3ec964d5c79b3f
Emhoff <075bct038@ioepc.edu.np>
2026-09-17 17:44:23 +0545

    Revert " setting database from start"
    This reverts commit dd6423c0b53943c3975a9b18817b3bf5a2bd6ef9.

--- app/db/schema.ts
+++
@@ -1,8 +0,0 @@
-import { integer, pgTable, PgVarcharBuilder, varchar} from "drizzle-orm/pg-core"
-
-export const userTable = pgTable("users", {
-    id:integer().primaryKey().generatedAlwaysAsIdentity(),
-    name:varchar({length:255}).notNull(),
-    age:integer().notNull(),
-    email: varchar({length:255}).notNull().unique()
-})
